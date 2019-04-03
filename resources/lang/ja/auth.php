2d2599bbbaefc227ba4b13c5cc623a6502fc2d9c
mikunilabo <k-wada@mikunilabo.com>
2019-04-03 22:21:59 +0900

    Added language files.

--- resources/lang/ja/auth.php
+++ resources/lang/ja/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed'   => '入力されたアカウント情報が間違っているか、認証を終えていないか、存在しないアカウントです。',
+    'throttle' => 'ログイン試行回数が一定回数を越えたため、アカウントがロックされました。:minutes 分経過後に再入力してください。',
 
 ];
