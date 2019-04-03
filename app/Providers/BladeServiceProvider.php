76103514d8d77714fbad08ebaeec23f9b94e4d38
mikunilabo <k-wada@mikunilabo.com>
2019-04-03 22:23:06 +0900

    Added blade directives.

---
+++ app/Providers/BladeServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+final class BladeServiceProvider extends ServiceProvider
+{
+    /**
+     * @return void
+     */
+    public function boot(): void
+    {
+        $this->extends();
+    }
+
+    /**
+     * @return void
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * @return void
+     */
+    private function extends(): void
+    {
+        /**
+         * @param string|array $env
+         * @return bool
+         */
+        Blade::if('env', function ($env) {
+            return app()->environment($env);
+        });
+
+        /**
+         * @return string
+         */
+        Blade::directive('csrf', function () {
+            return csrf_field();
+        });
+    }
+}
