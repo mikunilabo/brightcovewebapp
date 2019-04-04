2d662658f34acc63826e167541c8e9dde08eb04c
mikunilabo <k-wada@mikunilabo.com>
2019-04-04 17:18:05 +0900

    パスワードリマインダ・リセット画面のhtmlインデント調整等

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -4,40 +4,45 @@
 
 @section('content')
     <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-md-6">
-          <div class="card mx-4">
-            <form action="{{ route('password.email') }}" method="POST">
-                {{ csrf_field() }}
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card mx-4">
+                    <form action="{{ route('password.email') }}" method="POST">
+                        {{ csrf_field() }}
 
-                <div class="card-body p-4">
-                  <h1><i class="fa fa-send fa-fw"></i>@lang ('Reset Password')</h1>
-                  <p class="text-muted">@lang ('Please enter your registered e-mail address.')</p>
+                        <div class="card-body p-4">
+                            <h1 class="mb-3">
+                                <i class="fa fa-send fa-fw"></i>@lang ('Reset Password')
+                            </h1>
+                            <p class="text-muted">@lang ('Please enter your registered e-mail address.')</p>
 
-                  @if (session('status'))
-                      <div class="alert alert-success" role="alert">
-                          {{ session('status') }}
-                      </div>
-                  @endif
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">{{ session('status') }}</div>
+                            @endif
 
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text">@</span>
-                    </div>
-                    <input name="email" type="email" value="{{ old('email') }}" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" placeholder="@lang ('E-Mail')" required autofocus />
-                    @if ($errors->has('email'))
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $errors->first('email') }}</strong>
-                        </span>
-                    @endif
-                  </div>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">@</span>
+                                </div>
+                                <input name="email" type="email" value="{{ old('email') }}" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" placeholder="@lang ('E-Mail')" required autofocus />
 
-                  <button class="btn btn-block btn-outline-warning mt-2" type="{{ empty($demo) ? 'submit' : 'button' }}">@lang ('Submit')</button>
-                  <a href="{{ route('login') }}" class="btn btn-block btn-link px-0">@lang ('Back to sign in page.')</a>
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+
+                            <button class="btn btn-block btn-outline-warning mt-2" type="{{ empty($demo) ? 'submit' : 'button' }}">
+                                @lang ('Submit')
+                            </button>
+                            <a href="{{ route('login') }}" class="btn btn-block btn-link px-0">
+                                @lang ('Back to sign in page.')
+                            </a>
+                        </div>
+                    </form>
                 </div>
-            </form>
-          </div>
+            </div>
         </div>
-      </div>
     </div>
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -3,65 +3,71 @@
 @section('title', __('Reset Password'))
 
 @section('content')
-<div class="container">
-      <div class="row justify-content-center">
-        <div class="col-md-6">
-          <div class="card mx-4">
-            <form action="{{ route('password.request') }}" method="POST">
-                <input type="hidden" name="token" value="{{ $token }}">
-                {{ csrf_field() }}
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card mx-4">
+                    <form action="{{ route('password.request') }}" method="POST">
+                        <input type="hidden" name="token" value="{{ $token }}"> {{
+                        csrf_field() }}
 
-                <div class="card-body p-4">
-                  <h1><i class="fa fa-refresh fa-fw"></i>@lang ('Reset Password')</h1>
-                  <p class="text-muted">@lang ('Please enter the email address of the account you want to reset and the new password.')</p>
+                        <div class="card-body p-4">
+                            <h1 class="mb-3">
+                                <i class="fa fa-refresh fa-fw"></i>@lang ('Reset Password')
+                            </h1>
+                            <p class="text-muted">
+                                @lang ('Please enter the email address of the account you want to reset and the new password.')
+                            </p>
 
-                  @if (session('status'))
-                      <div class="alert alert-success" role="alert">
-                          {{ session('status') }}
-                      </div>
-                  @endif
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">{{ session('status') }}</div>
+                            @endif
 
-                  <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text">@</span>
-                    </div>
-                    <input name="email" type="email" value="{{ old('email') }}" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" placeholder="@lang ('E-Mail')" required />
-                    @if ($errors->has('email'))
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $errors->first('email') }}</strong>
-                        </span>
-                    @endif
-                  </div>
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">@</span>
+                                </div>
+                                <input name="email" type="email" value="{{ old('email') }}" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" placeholder="@lang ('E-Mail')" required />
 
-                  <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text">
-                        <i class="icon-lock"></i>
-                      </span>
-                    </div>
-                    <input name="password" type="password" value class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" placeholder="@lang ('Password')" required />
-                    @if ($errors->has('password'))
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $errors->first('password') }}</strong>
-                        </span>
-                    @endif
-                  </div>
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
 
-                  <div class="input-group mb-4">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text">
-                        <i class="icon-lock"></i>
-                      </span>
-                    </div>
-                    <input name="password_confirmation" type="password" value class="form-control {{ $errors->has('password_confirmation') ? 'is-invalid' : '' }}" placeholder="@lang ('Repeat password')" required />
-                  </div>
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="icon-lock"></i>
+                                    </span>
+                                </div>
+                                <input name="password" type="password" value class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" placeholder="@lang ('Password')" required />
 
-                  <button class="btn btn-block btn-outline-danger" type="{{ empty($demo) ? 'submit' : 'button' }}">@lang ('Reset')</button>
-                  <a href="{{ route('login') }}" class="btn btn-block btn-link px-0">@lang ('Back to sign in page.')</a>
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+
+                            <div class="input-group mb-4">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"> <i class="icon-lock"></i>
+                                    </span>
+                                </div>
+                                <input name="password_confirmation" type="password" value class="form-control {{ $errors->has('password_confirmation') ? 'is-invalid' : '' }}" placeholder="@lang ('Repeat password')" required />
+                            </div>
+
+                            <button class="btn btn-block btn-outline-danger" type="{{ empty($demo) ? 'submit' : 'button' }}">
+                                @lang ('Reset')
+                            </button>
+                            <a href="{{ route('login') }}" class="btn btn-block btn-link px-0">
+                                @lang ('Back to sign in page.')
+                            </a>
+                        </div>
+                    </form>
                 </div>
-            </form>
-          </div>
+            </div>
         </div>
-      </div>
     </div>
 @endsection
