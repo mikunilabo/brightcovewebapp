194743fcd31bb5169a4dc81ad6446e7ab4546e28
mikunilabo <k-wada@mikunilabo.com>
2019-04-09 16:57:04 +0900

    * README更新 * その他メタ修正

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,14 +1,14 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-<meta name="description" content="Mikunilabo Admin Demo">
-<meta name="author" content="Kuniyasu Wada">
+<meta name="description" content="">
+<meta name="author" content="">
 <meta name="keyword" content="">
 
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<title>@yield ('title') - {{ config('app.name', 'Laravel') }}</title>
+<title>@yield ('title') - {{ config('app.name') }}</title>
 
 <!-- Styles -->
 <link href="{{ asset(mix('css/app.css')) }}" rel="stylesheet">
