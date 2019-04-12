99ec7fb82ccb83e5f8d7a3486d446f1dda9f750c
mikunilabo <k-wada@mikunilabo.com>
2019-04-12 23:29:01 +0900

    Add authorize gate.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 final class AuthServiceProvider extends ServiceProvider
@@ -22,6 +23,13 @@ final class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('authorize', function (Authenticatable $user, ...$args): bool {
+            foreach (is_array($args) ? $args : [$args] as $arg) {
+                if ($user->role->permissions->containsStrict('slug', $arg)) {
+                    return true;
+                }
+            }
+            return false;
+        });
     }
 }
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -9,43 +9,57 @@
 
             <li class="nav-title">@lang ('Main menu')</li>
 
-            <!-- Media -->
-            <li class="nav-item nav-dropdown">
-                <a class="nav-link nav-dropdown-toggle" href="#">
-                    <i class="nav-icon icon-playlist"></i>@lang ('Media')
-                </a>
-                <ul class="nav-dropdown-items">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"> <i
-                            class="nav-icon icon-list"></i>@lang ('Media list')
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"> <i
-                            class="nav-icon icon-cloud-upload"></i>@lang ('Media upload')
-                        </a>
-                    </li>
-                </ul>
-            </li>
+            @can ('authorize', ['media-select', 'media-upload'])
+                <!-- Media -->
+                <li class="nav-item nav-dropdown">
+                    <a class="nav-link nav-dropdown-toggle" href="#">
+                        <i class="nav-icon icon-playlist"></i>@lang ('Media')
+                    </a>
+                    <ul class="nav-dropdown-items">
+                        @can ('authorize', ['media-select'])
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <i class="nav-icon icon-list"></i>@lang ('Media list')
+                                </a>
+                            </li>
+                        @endcan
 
-            <!-- Accounts -->
-            <li class="nav-item nav-dropdown">
-                <a class="nav-link nav-dropdown-toggle" href="#">
-                    <i class="nav-icon icon-people"></i>@lang ('Accounts')
-                </a>
-                <ul class="nav-dropdown-items">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"> <i
-                            class="nav-icon icon-list"></i>@lang ('Accounts list')
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"> <i
-                            class="nav-icon icon-user-follow"></i>@lang ('Create account')
-                        </a>
-                    </li>
-                </ul>
-            </li>
+                        @can ('authorize', ['media-upload'])
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <i class="nav-icon icon-cloud-upload"></i>@lang ('Media upload')
+                                </a>
+                            </li>
+                        @endcan
+                    </ul>
+                </li>
+            @endcan
+
+            @can ('authorize', ['account-select', 'account-create'])
+                <!-- Accounts -->
+                <li class="nav-item nav-dropdown">
+                    <a class="nav-link nav-dropdown-toggle" href="#">
+                        <i class="nav-icon icon-people"></i>@lang ('Accounts')
+                    </a>
+                    <ul class="nav-dropdown-items">
+                        @can ('authorize', ['account-select'])
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <i class="nav-icon icon-list"></i>@lang ('Accounts list')
+                                </a>
+                            </li>
+                        @endcan
+
+                        @can ('authorize', ['account-create'])
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <i class="nav-icon icon-user-follow"></i>@lang ('Create account')
+                                </a>
+                            </li>
+                        @endcan
+                    </ul>
+                </li>
+            @endcan
         </ul>
     </nav>
     <button class="sidebar-minimizer brand-minimizer" type="button"></button>
