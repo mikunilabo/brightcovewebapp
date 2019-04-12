bd14795ea8c2e1caca053b7edc0c08a7a4d4cc5a
mikunilabo <k-wada@mikunilabo.com>
2019-04-12 16:15:29 +0900

    Add some seeds.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,12 +6,11 @@ use Illuminate\Database\Seeder;
 final class DatabaseSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
-     *
      * @return void
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(RolesSeeder::class);
+        $this->call(UsersSeeder::class);
     }
 }
---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+use App\Model\Eloquent\User;
+use App\Traits\Database\Transactionable;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    use Transactionable;
+
+    /** @var array */
+    private $items = [];
+
+    /**
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->items = [
+            [
+                'name' => 'Test user',
+                'email' => config('app.user'),
+                'password' => bcrypt(config('app.password')),
+                'role_id' => 1,
+            ],
+        ];
+    }
+
+    /**
+     * @return void
+     */
+    public function run()
+    {
+        try {
+            $this->transaction(function () {
+                collect($this->items)->each(function ($item) {
+                    User::create($item);
+                });
+            });
+        } catch (\Exception $e) {
+            report($e);
+            dd($e->getMessage());
+        }
+    }
+}
