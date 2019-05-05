69ede95909010d39571c9be7ab2fc2bcf4e94542
mikunilabo <k-wada@mikunilabo.com>
2019-05-05 22:13:00 +0900

    Media list, right menu from account list page, abolished checkbox.

--- resources/views/media/index.blade.php
+++ resources/views/media/index.blade.php
@@ -26,23 +26,16 @@
                                 <table class="table table-responsive-sm table-striped table-hover" id="media-table">
                                     <thead>
                                         <tr>
-                                            <th>
-                                                <input type="checkbox" />
-                                            </th>
                                             <th>@lang ('ID')</th>
                                             <th>@lang ('Title')</th>
                                             <th>@lang ('Status')</th>
                                             <th>@lang ('Created At')</th>
                                             <th>@lang ('Updated At')</th>
-                                            <th><i class="nav-icon icon-options"></i></th>
                                         </tr>
                                     </thead>
                                     <tbody id="tbody" class="d-none">
                                         @foreach ($rows as $row)
-                                            <tr>
-                                                <td>
-                                                    <input type="checkbox" />
-                                                </td>
+                                            <tr id="{{ $row->id }}">
                                                 <td><code>{{ $row->id }}</code></td>
                                                 <td>
                                                     <a href="{{ route('media.detail', $row->id) }}">{{ $row->name }}</a>
@@ -58,48 +51,44 @@
                                                     @set ($attribute, 'updated_at')
                                                     {{ is_null($row->{$attribute}) ? null : now()->parse($row->{$attribute})->setTimezone(config('app.timezone')) }}
                                                 </td>
-                                                <td>
-                                                    <div class="nav navbar-nav">
-                                                        <div class="nav-item dropdown">
-                                                            <a class="nav-link p-0" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                                                                <i class="nav-icon icon-options"></i>
-                                                            </a>
-
-                                                            <div class="dropdown-menu dropdown-menu-right">
-                                                                @can ('select', $row)
-                                                                    <a class="dropdown-item" href="{{ route('media.detail', $row->id) }}">
-                                                                        <i class="icons icon-note"></i>@lang ('Detail')
-                                                                    </a>
-                                                                @endcan
-
-                                                                @can ('delete', $row)
-                                                                    <a class="dropdown-item text-danger" href="{{ route('media.delete', $row->id) }}" onclick="event.preventDefault(); if (confirm('@lang ('Are you sure you want to delete this :name?', ['name' => __('Media')])')) { window.Common.submitForm('{{ route('media.delete', $row->id) }}'); } return false;">
-                                                                        <i class="icons icon-trash text-danger"></i>@lang ('Delete')
-                                                                    </a>
-                                                                @endcan
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </td>
                                             </tr>
                                         @endforeach
                                     </tbody>
                                 </table>
-
-                                {{--{!! $rows->render() !!}--}}
                             </div>
                             <div class="card-footer">
-                                <a class="btn btn-secondary btn-sm float-left dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                                    @lang ('Batch operation')
-                                </a>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item text-danger" href="#" onclick="event.preventDefault(); if (confirm('@lang("test?")')) console.log('entered.'); return false;">
-                                        <i class="icons icon-trash text-danger"></i>@lang ('Delete')
+                                @can ('authorize', ['media-delete', 'media-update'])
+                                    <a href="#" class="btn btn-secondary btn-sm float-left dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                        @lang ('Batch operation')
                                     </a>
-                                </div>
+                                @endcan
+
+                                @can ('authorize', ['media-delete', 'media-update'])
+                                    <div class="dropdown-menu" disabled>
+                                        @can ('authorize', 'media-update')
+                                            <a href="#" id="activate-btn" class="dropdown-item text-success disabled" onclick="event.preventDefault(); if (confirm('@lang ('Are you sure you want to :action the selected :name?', ['name' => __('Media'), 'action' => __('Activate')])')) {  } return false;">
+                                                <i class="icons icon-share text-success"></i>@lang ('Activate')
+                                            </a>
+                                        @endcan
+
+                                        @can ('authorize', 'media-update')
+                                            <a href="#" id="deactivate-btn" class="dropdown-item disabled" onclick="event.preventDefault(); if (confirm('@lang ('Are you sure you want to :action the selected :name?', ['name' => __('Media'), 'action' => __('Deactivate')])')) {  } return false;">
+                                                <i class="icons icon-ban"></i>@lang ('Deactivate')
+                                            </a>
+                                        @endcan
+
+                                        <div class="dropdown-divider"></div>
+
+                                        @can ('authorize', 'media-delete')
+                                            <a href="#" id="delete-btn" class="dropdown-item text-danger disabled" onclick="event.preventDefault(); if (confirm('@lang ('Are you sure you want to :action the selected :name?', ['name' => __('Media'), 'action' => __('Delete')])')) {  } return false;">
+                                                <i class="icons icon-trash text-danger"></i>@lang ('Delete')
+                                            </a>
+                                        @endcan
+                                    </div>
+                                @endcan
 
                                 @can ('authorize', 'media-create')
-                                    <a class="btn btn-primary btn-sm float-right" href="{{ route('media.upload') }}">
+                                    <a href="{{ route('media.upload') }}" class="btn btn-primary btn-sm float-right">
                                         <i class="nav-icon icon-cloud-upload"></i> @lang ('Media Upload')
                                     </a>
                                 @endcan
@@ -119,35 +108,57 @@
         window.Common.overlay();
 
         $(document).ready(function () {
-            $.extend( $.fn.dataTable.defaults, {
+            $.extend($.fn.dataTable.defaults, {
                 language: {
                     url: "{{ asset('vendor/DataTables/ja.json') }}"
                 }
             });
-            $('#media-table').DataTable({
-                columnDefs: [
-                    {
-                        targets: [0, 6],
-                        orderable: false
-                    }
-                ],
-                displayLength: 25,
-                "drawCallback": function() {
+
+            var table = $('#media-table').DataTable({
+                columnDefs: [],
+                displayLength: 20,
+                'drawCallback': function () {
                     window.Common.overlayOut();
                     document.getElementById('tbody').classList.remove('d-none');
                 },
                 info: true,
                 lengthChange: true,
-                lengthMenu: [10, 25, 50, 100],
+                lengthMenu: [10, 20, 30, 50],
                 order: [
-                    [4, 'desc'],
-                	],
+                    [3, 'desc'],
+                ],
                 ordering: true,
                 paging: true,
                 scrollX: false,
                 searching: true,
+                select: {
+                    style: 'multi',
+                    selector: 'tr',
+                    blurable: false,
+                },
                 stateSave: true,
                 responsive: true,
+            });
+
+            $('#delete-btn').click(function () {
+                var ids = table.rows('.selected').ids();
+                console.log(ids.toArray());
+            });
+
+            table
+            .on('select', function (e, dt, type, indexes) {
+                if (type === 'row' && table.rows('.selected').data().length > 0) {
+                    document.getElementById('activate-btn').classList.remove('disabled');
+                    document.getElementById('deactivate-btn').classList.remove('disabled');
+                    document.getElementById('delete-btn').classList.remove('disabled');
+                }
+            })
+            .on('deselect', function (e, dt, type, indexes) {
+                if (type === 'row' && table.rows('.selected').data().length === 0) {
+                    document.getElementById('activate-btn').classList.add('disabled');
+                    document.getElementById('deactivate-btn').classList.add('disabled');
+                    document.getElementById('delete-btn').classList.add('disabled');
+                }
             });
         });
     </script>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -26,24 +26,18 @@
                                 <table class="table table-responsive-sm table-striped table-hover" id="users-table">
                                     <thead>
                                         <tr>
-                                            <th>
-                                                <input type="checkbox" />
-                                            </th>
                                             <th>@lang ('Name')</th>
                                             <th>@lang ('ID')</th>
                                             <th>@lang ('Company')</th>
                                             <th>@lang ('Role')</th>
                                             <th>@lang ('E-Mail')</th>
                                             <th>@lang ('Last login')</th>
-                                            <th><i class="nav-icon icon-options"></i></th>
+                                            <th>@lang ('Created At')</th>
                                         </tr>
                                     </thead>
-                                    <tbody>
+                                    <tbody id="tbody" class="d-none">
                                         @foreach ($rows as $row)
-                                            <tr>
-                                                <td>
-                                                    <input type="checkbox" />
-                                                </td>
+                                            <tr id="{{ $row->id }}">
                                                 <td>
                                                     <a href="{{ route('accounts.detail', $row->id) }}">{{ $row->name }}</a>
                                                 </td>
@@ -58,46 +52,13 @@
                                                     </a>
                                                 </td>
                                                 <td>{{ is_null($createdAt = optional($row->loginHistories->first())->created_at) ? '-' : $createdAt->fuzzy() }}</td>
-                                                <td>
-                                                    <div class="nav navbar-nav">
-                                                        <div class="nav-item dropdown">
-                                                            <a class="nav-link p-0" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                                                                <i class="nav-icon icon-options"></i>
-                                                            </a>
-
-                                                            <div class="dropdown-menu dropdown-menu-right">
-                                                                @can ('select', $row)
-                                                                    <a class="dropdown-item" href="{{ route('accounts.detail', $row->id) }}">
-                                                                        <i class="icons icon-note"></i>@lang ('Detail')
-                                                                    </a>
-                                                                @endcan
-
-                                                                @can ('delete', $row)
-                                                                    <a class="dropdown-item text-danger" href="{{ route('accounts.delete', $row->id) }}" onclick="event.preventDefault(); if (confirm('@lang ('Are you sure you want to delete this :name?', ['name' => __('Account')])')) { window.Common.submitForm('{{ route('accounts.delete', $row->id) }}'); } return false;">
-                                                                        <i class="icons icon-trash text-danger"></i>@lang ('Delete')
-                                                                    </a>
-                                                                @endcan
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </td>
+                                                <td>{{ $row->created_at }}</td>
                                             </tr>
                                         @endforeach
                                     </tbody>
                                 </table>
-
-                                {{--{!! $rows->render() !!}--}}
                             </div>
                             <div class="card-footer">
-                                <a class="btn btn-secondary btn-sm float-left dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                                    @lang ('Batch operation')
-                                </a>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item text-danger" href="#" onclick="event.preventDefault(); if (confirm('@lang("test?")')) console.log('entered.'); return false;">
-                                        <i class="icons icon-trash text-danger"></i>@lang ('Delete')
-                                    </a>
-                                </div>
-
                                 @can ('authorize', 'user-create')
                                     <a class="btn btn-primary btn-sm float-right" href="{{ route('accounts.create') }}">
                                         <i class="nav-icon icon-user-follow"></i> @lang ('Create account')
@@ -116,28 +77,37 @@
     @parent
 
     <script type="text/javascript">
+        window.Common.overlay();
+
         $(document).ready(function () {
-            $.extend( $.fn.dataTable.defaults, {
+            $.extend($.fn.dataTable.defaults, {
                 language: {
                     url: "{{ asset('vendor/DataTables/ja.json') }}"
                 }
             });
-            $('#users-table').DataTable({
-                columnDefs: [
-                    {
-                        targets: [0, 7],
-                        orderable: false
-                    }
-                ],
-                displayLength: 25,
+
+            var table = $('#users-table').DataTable({
+                columnDefs: [],
+                displayLength: 20,
+                'drawCallback': function () {
+                    window.Common.overlayOut();
+                    document.getElementById('tbody').classList.remove('d-none');
+                },
                 info: true,
                 lengthChange: true,
-                lengthMenu: [10, 25, 50, 100],
-                order: [],
+                lengthMenu: [10, 20, 30, 50],
+                order: [
+                    [6, 'desc'],
+                ],
                 ordering: true,
                 paging: true,
                 scrollX: false,
                 searching: true,
+                select: {
+                    style: 'multi',
+                    selector: 'tr',
+                    blurable: false,
+                },
                 stateSave: true,
                 responsive: true,
             });
