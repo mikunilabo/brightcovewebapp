17971541b505df9fcd0571f3f30b38493f72626f
mikunilabo <k-wada@mikunilabo.com>
2019-05-22 10:49:27 +0900

    Restricted console output except when abnormal.

--- resources/js/VideoCloud.js
+++ resources/js/VideoCloud.js
@@ -7,22 +7,22 @@ class VideoCloud {
   operationMediaWithSource = async (mediaObject, callback) => {
     if (this.mediaId) {
       var media = await this.updateMedia(mediaObject);
-      console.log("%cMedia Updated", "background: #F90; color: #FFF", media);
+      console.log("%cMedia Updated.", "background: #F90; color: #FFF"/*, media*/);
     } else {
       var media = await this.createMedia(mediaObject);
       this.mediaId = media.id;
-      console.log("%cMedia Created", "background: #F90; color: #FFF", media);
+      console.log("%cMedia Created.", "background: #F90; color: #FFF"/*, media*/);
     }
 
     if (this.source) {
       const s3Url = await this.getS3Url(this.mediaId, this.source.name);
-      console.log("%cS3 URL Fetched", "background: #990; color: #FFF", s3Url);
+      console.log("%cS3 URL Fetched.", "background: #990; color: #FFF"/*, s3Url*/);
 
       const uploadResponse = await window.Uploader.multiPartUpload(this.source, s3Url);
-      console.log("%cFile Uploaded", "background: #99F; color: #FFF", uploadResponse);
+      console.log("%cFile Uploaded.", "background: #99F; color: #FFF"/*, uploadResponse*/);
 
       const ingestResponse = await this.dynamicIngest(this.mediaId, s3Url.api_request_url);
-      console.log("%cIngest Started", "background: #F0F; color: #FFF", ingestResponse);
+      console.log("%cIngest Requested.", "background: #F0F; color: #FFF"/*, ingestResponse*/);
     }
 
     callback(media);
@@ -77,7 +77,7 @@ class VideoCloud {
   };
 
   invalidFeedback = response => {
-    console.log(response.data.errors);
+    console.error(response.data.errors);
 
     [].slice.call(document.getElementsByClassName('invalid-feedback')).forEach(function(span) {
       window.Common.removeChildren(span);
