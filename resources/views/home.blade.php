47c983d816004ec3d93059669f31702ed933938b
mikunilabo <k-wada@mikunilabo.com>
2019-05-25 00:09:26 +0900

    Provision temporary copy function to clipboard when clicked.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,11 @@
             <div class="row">
                 <div class="col-sm-12 col-md-12 col-lg-12">
                     <button type="button" id="toastr" class="btn btn-info">toastr</button>
+
+                    @component ('components.popovers.informations', ['content' => __('Click to copy to clipboard.')]) @endcomponent
+                    <input type="text" id="input" class="form-control" value="testtest" readonly />
+
+                    <textarea id="result" class="form-control" disabled></textarea>
                 </div>
             </div>
         </div>
@@ -27,11 +32,9 @@
 
     <script type="text/javascript">
       document.getElementById('toastr').addEventListener('click', function (event) {
-        window.toastr.info('クリップボードにコピーしました。');
-        // Display a warning toast, with no title
+        window.toastr.info('My name is Inigo Montoya. You killed my father, prepare to die!');
         window.toastr.warning('My name is Inigo Montoya. You killed my father, prepare to die!');
-
-        // Display a success toast, with a title
+        window.toastr.error('I do not think that word means what you think it means.', 'Inconceivable!');
         window.toastr.success('Have fun storming the castle!', 'Miracle Max Says', {
           "closeButton": true,
           "debug": false,
@@ -49,14 +52,24 @@
           "hideMethod": "fadeOut"
         });
 
-        // Display an error toast, with a title
-        window.toastr.error('I do not think that word means what you think it means.', 'Inconceivable!');
-
         // Immediately remove current toasts without using animation
         // window.toastr.remove()
 
         // Remove current toasts using animation
         // window.toastr.clear()
       });
+    </script>
+
+    <script type="text/javascript">
+      (function () {
+        'use strict';
+
+        var input = document.getElementById('input');
+        input.addEventListener('click' , function(e){
+          input.select();
+          document.execCommand('copy');
+          window.toastr.info(window.lang['Copied it to the clipboard.']);
+        });
+      })();
     </script>
 @endsection
