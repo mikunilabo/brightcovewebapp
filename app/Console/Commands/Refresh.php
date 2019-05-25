f4ed45a00bb81bab09c3a0ee30b1fcfacd15ab1e
mikunilabo <k-wada@mikunilabo.com>
2019-05-25 17:23:51 +0900

    Added auto load option to Refresh command. Output content adjustment to the console.

--- app/Console/Commands/Refresh.php
+++ app/Console/Commands/Refresh.php
@@ -4,31 +4,36 @@ declare(strict_types=1);
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Composer;
 
 final class Refresh extends Command
 {
     /**
-     * The name and signature of the console command.
-     *
      * @var string
      */
-    protected $signature = 'refresh {--c|cache : Cache again after deleting.}';
+    protected $signature = 'refresh
+                        {--c|cache : Cache again after deleting.}
+                        {--d|dumpautoload : Perform autoloading with composer.}';
 
     /**
-     * The console command description.
-     *
      * @var string
      */
     protected $description = 'Execute the refresh commands.';
 
     /**
-     * Create a new command instance.
-     *
+     * @var Composer
+     */
+    private $composer;
+
+    /**
+     * @param Composer $composer
      * @return void
      */
-    public function __construct()
+    public function __construct(Composer $composer)
     {
         parent::__construct();
+
+        $this->composer = $composer;
     }
 
     /**
@@ -38,19 +43,47 @@ final class Refresh extends Command
      */
     public function handle()
     {
+        $jobs = 7;
+        $jobs += $this->option('dumpautoload') ? 1 : 0;
+        $progress = $this->output->createProgressBar($jobs);
+
         $this->comment('Refreshing...');
-        $this->call('clear-compiled');
-        $this->call('cache:clear');
+
+        $this->callSilent('down');
+        $progress->advance();
+
+        $this->callSilent('clear-compiled');
+        $progress->advance();
+
+        $this->callSilent('cache:clear');
+        $progress->advance();
+
+        $this->callSilent('view:clear');
+        $progress->advance();
 
         if ($this->option('cache')) {
-            $this->call('config:cache');
-            $this->call('route:cache');
+            $this->callSilent('config:cache');
+            $progress->advance();
+
+            $this->callSilent('route:cache');
+            $progress->advance();
         } else {
-            $this->call('config:clear');
-            $this->call('route:clear');
+            $this->callSilent('config:clear');
+            $progress->advance();
+
+            $this->callSilent('route:clear');
+            $progress->advance();
         }
 
-        $this->call('view:clear');
-        $this->comment('Refresh commands executed!');
+        if ($this->option('dumpautoload')) {
+            $this->composer->dumpAutoloads();
+            $progress->advance();
+        }
+
+        $this->callSilent('up');
+        $progress->finish();
+
+        $this->line('');
+        $this->comment('Some refresh commands are executed!');
     }
 }
