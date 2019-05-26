eece9c8d4ee0fd68b082e15aec34551f5113eba5
mikunilabo <k-wada@mikunilabo.com>
2019-05-26 23:55:18 +0900

    Provisional coding of notification area to header.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,12 +15,14 @@
         <div class="container-fluid animated fadeIn">
             <div class="row">
                 <div class="col-sm-12 col-md-12 col-lg-12">
+                    @if (false)
                     <button type="button" id="toastr" class="btn btn-info">toastr</button>
 
                     @component ('components.popovers.informations', ['content' => __('Click to copy to clipboard.')]) @endcomponent
                     <input type="text" id="input" class="form-control" value="testtest" readonly />
 
                     <textarea id="result" class="form-control" disabled></textarea>
+                    @endif
                 </div>
             </div>
         </div>
@@ -31,26 +33,37 @@
     @parent
 
     <script type="text/javascript">
-      document.getElementById('toastr').addEventListener('click', function (event) {
-        window.toastr.info('My name is Inigo Montoya. You killed my father, prepare to die!');
-        window.toastr.warning('My name is Inigo Montoya. You killed my father, prepare to die!');
-        window.toastr.error('I do not think that word means what you think it means.', 'Inconceivable!');
-        window.toastr.success('Have fun storming the castle!', 'Miracle Max Says', {
-          "closeButton": true,
-          "debug": false,
-          "newestOnTop": false,
-          "progressBar": false,
-          "positionClass": "toast-bottom-right",
-          "preventDuplicates": false,
-          "showDuration": "300",
-          "hideDuration": "1000",
-          "timeOut": "5000",
-          "extendedTimeOut": "1000",
-          "showEasing": "swing",
-          "hideEasing": "linear",
-          "showMethod": "fadeIn",
-          "hideMethod": "fadeOut"
-        });
+      (function () {
+        'use strict';
+
+        // var input = document.getElementById('input');
+        // input.addEventListener('click' , function(e){
+        //   input.select();
+        //   document.execCommand('copy');
+        //   window.toastr.info(window.lang['Copied it to the clipboard.']);
+        // });
+      })();
+
+      // document.getElementById('toastr').addEventListener('click', function (event) {
+      //   window.toastr.info('My name is Inigo Montoya. You killed my father, prepare to die!');
+      //   window.toastr.warning('My name is Inigo Montoya. You killed my father, prepare to die!');
+      //   window.toastr.error('I do not think that word means what you think it means.', 'Inconceivable!');
+      //   window.toastr.success('Have fun storming the castle!', 'Miracle Max Says', {
+      //     "closeButton": true,
+      //     "debug": false,
+      //     "newestOnTop": false,
+      //     "progressBar": false,
+      //     "positionClass": "toast-bottom-right",
+      //     "preventDuplicates": false,
+      //     "showDuration": "300",
+      //     "hideDuration": "1000",
+      //     "timeOut": "5000",
+      //     "extendedTimeOut": "1000",
+      //     "showEasing": "swing",
+      //     "hideEasing": "linear",
+      //     "showMethod": "fadeIn",
+      //     "hideMethod": "fadeOut"
+      //   });
 
         // Immediately remove current toasts without using animation
         // window.toastr.remove()
@@ -58,18 +71,5 @@
         // Remove current toasts using animation
         // window.toastr.clear()
       });
-    </script>
-
-    <script type="text/javascript">
-      (function () {
-        'use strict';
-
-        var input = document.getElementById('input');
-        input.addEventListener('click' , function(e){
-          input.select();
-          document.execCommand('copy');
-          window.toastr.info(window.lang['Copied it to the clipboard.']);
-        });
-      })();
     </script>
 @endsection
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -11,6 +11,45 @@
     </button>
 
     <ul class="nav navbar-nav ml-auto">
+        @if (false)
+            <li class="nav-item dropdown d-md-down-none">
+                <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                    <i class="icons icon-bell"></i>
+                    <span class="badge badge-pill badge-danger">4</span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right dropdown-menu-lg">
+                    <div class="dropdown-header text-center">
+                        <strong>You have 4 messages</strong>
+                    </div>
+
+                    @for ($i = 0; $i < 4; $i++)
+                        <a class="dropdown-item" href="#">
+                            <div class="message">
+                                <div class="py-2 mr-3 float-left">
+                                    <div class="avatar">
+                                        <img class="img-avatar" src="{{ config('resources.images.logo') }}" alt="">
+                                        <span class="avatar-status badge-info"></span>
+                                    </div>
+                                </div>
+                                <div>
+                                    <small class="text-muted">Sender name</small>
+                                    <small class="text-muted float-right mt-1">Just now</small>
+                                </div>
+                                <div class="text-truncate font-weight-bold">
+                                    Title
+                                </div>
+                                <div class="small text-muted text-truncate">Contents...</div>
+                            </div>
+                        </a>
+                    @endfor
+
+                    <a class="dropdown-item text-center" href="#">
+                    <strong>View all messages</strong>
+                    </a>
+                </div>
+            </li>
+        @endif
+
         <li class="nav-item dropdown">
             <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
                 {{ Auth::user()->name }} <i class="fa fa-caret-down"></i>
