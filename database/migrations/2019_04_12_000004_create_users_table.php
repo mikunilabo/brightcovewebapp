4067f9303dd94f88e6648b0a1138fbb95fe2e3b7
mikunilabo <k-wada@mikunilabo.com>
2019-05-30 15:58:20 +0900

    Changed the method to handle logical deletion while maintaining unique and NOT NULL definition of email column.

--- database/migrations/2019_04_12_000004_create_users_table.php
+++ database/migrations/2019_04_12_000004_create_users_table.php
@@ -22,12 +22,20 @@ final class CreateUsersTable extends Migration
                 $table->uuid('id')->primary();
                 $table->string('name');
                 $table->string('company')->nullable();
-                $table->string('email')->nullable()->unique();
+                $table->string('email');
                 $table->unsignedInteger('role_id');
+                $table->boolean('existence')
+                    ->nullable()
+                    ->storedAs('CASE WHEN deleted_at IS NULL THEN 1 ELSE NULL END');
                 $table->string('password');
                 $table->rememberToken();
                 $table->timestamps();
                 $table->softDeletes();
+
+                $table->unique([
+                    'email',
+                    'existence',
+                ]);
 
                 $table->foreign('role_id')
                     ->references('id')
