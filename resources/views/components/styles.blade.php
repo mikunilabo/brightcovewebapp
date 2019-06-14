aed008083f8ecc12ab51c11e9ac5f65d82cb005b
mikunilabo <k-wada@mikunilabo.com>
2019-06-14 23:58:59 +0900

    Initial assets are prepared for the condition before front end build.

--- resources/views/components/styles.blade.php
+++ resources/views/components/styles.blade.php
@@ -1,3 +1,7 @@
 @push ('styles.app')
-    <link href="{{ asset(mix('css/app.css')) }}" rel="stylesheet">
+    @if (file_exists(public_path('mix-manifest.json')))
+        <link href="{{ asset(mix('css/app.css')) }}" rel="stylesheet">
+    @else
+        <link href="{{ asset('css/default.css') }}" rel="stylesheet">
+    @endif
 @endpush
