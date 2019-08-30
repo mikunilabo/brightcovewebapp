3267d2bded4481580ef0865315d882fdc4ae60dc
mikunilabo <k-wada@mikunilabo.com>
2019-08-30 14:46:11 +0900

    Various adjustments for heroku deployment.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,4 +2,9 @@
     <div>
         <span>&copy;{{ now()->year !== 2019 ? '2019 - ' : '' }}{{ now()->year }} {{ config('app.copyright') }}</span>
     </div>
+
+    <div class="ml-auto">
+        <span>Designed by</span>
+        <a href="https://www.mikunilabo.com/" target="_blank">MikuniLabo</a>
+    </div>
 </footer>
